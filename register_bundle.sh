ff8666181db677b108faa41192a1f5163e8a3ae3
vca4 <144377680+vca4@users.noreply.github.com>
2025-12-18 13:46:50 +0000

    Delete register_bundle.sh

--- register_bundle.sh
+++
@@ -1,8 +0,0 @@
-set -e
-NAME="$1"
-VERSION="$2"
-ARCHIVE="$3"
-mysql -udeployuser -pSecure123 deploydb <<EOF
-INSERT INTO bundles (name,version,archive,status)
-VALUES('$NAME','$VERSION','$ARCHIVE','new');
-EOF
